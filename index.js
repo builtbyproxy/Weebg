ea0f8c880c3a2ac46ad1f823e868715da7c24b48
builtbyproxy <lachlan_young@hotmail.com>
2017-09-06 17:29:16 +1000

    Randomly Changing Wallpaper, still static, scraper will be implemented eventually, most likely wallhaven

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 #! /usr/bin/env node
 
 var wallpaper = require('wallpaper');
+var http = require('http');
+var fs = require('fs');
+
+var imgUrls = ["http://cdn.wallpapersafari.com/89/55/2VKyOb.jpg", "http://www.misucell.com/data/out/9/IMG_340822.png", "http://wallpaperscraft.com/image/cat_girl_nekomimi_art_anime_girl_103991_1920x1080.jpg"];
+var imgUrl = "";
+var imgName = "weebgReferenceBackground" + imgUrl.substring(imgUrl.length - 4);
 
 var pathToModule = require('path').dirname(require.main.filename);
-var imgName = "weebgReferenceBackground.jpg";
 var path = pathToModule + "/" + imgName;
+
+function setImgUrl(){
+    var i = Math.floor(Math.random() * (imgUrls.length - 0 + 1)) + 0;
+        if(i >= imgUrls.length){i--;}
+
+    //Append
+    imgUrl = imgUrls[i];
+}
+
+function setImgName(){
+    setImgUrl();
+
+    var file = fs.createWriteStream(imgName);
+    var request = http.get(imgUrl, function(response) {
+        response.pipe(file);
+    });
+
+    console.log("1. Image Should Be Located")
+}
+
+function saveWallpaper()
+{
+    fs.stat(imgName, function(err, stat) {
+        if(err == null)
+        {
+            fs.unlinkSync(path);
+            setImgName();
+        }
+        else if(err.code == 'ENOENT')
+        {
+            setImgName();
+        }
+    });
+
+    console.log("2. Image Should Be Saved");
+
+    setWallpaper();
+}
 
 function setWallpaper()
 {
-    console.log("Path To BG Image: " + path);
     wallpaper.set(path);
-    console.log("Background Successfully Changed!");
-
+    console.log("3. Image Should Be Set");
 }
 
-setWallpaper();
+saveWallpaper();
