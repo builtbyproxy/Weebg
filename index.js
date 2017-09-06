2132e0bc170ae6cb079dfba85bd2b13847283c3e
builtbyproxy <lachlan_young@hotmail.com>
2017-09-06 20:28:40 +1000

    Dynamic Aync-Await Working

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 #! /usr/bin/env node
 
 var wallpaper = require('wallpaper');
+var fs = require('fs');
+var request = require('request');
+
+var imgUrls = ["http://cdn.wallpapersafari.com/89/55/2VKyOb.jpg", "http://www.misucell.com/data/out/9/IMG_340822.png", "http://wallpaperscraft.com/image/cat_girl_nekomimi_art_anime_girl_103991_1920x1080.jpg"];
+var imgUrl = ""
+var imgName = "";
 
 var pathToModule = require('path').dirname(require.main.filename);
-<<<<<<< HEAD
 var path = "";
+
 
 function setImgUrl(){
     var i = Math.floor(Math.random() * (imgUrls.length - 0 + 1)) + 0;
@@ -17,50 +23,37 @@ function setImgUrl(){
 
 async function setImgName(){
     setImgUrl();
-    var path = pathToModule + "/" + imgName;
-    var file = fs.createWriteStream(path);
 
-    console.log(path);
-
-    var response = await http.get(imgUrl);
-    response.pipe(file);
-
-    //
-    // var request = await http.get(imgUrl, function(response) {
-    //     response.pipe(file);
-    // });
+    path = pathToModule + "/" + imgName;
+    request(imgUrl).pipe(fs.createWriteStream(path));
 
     console.log("1. Image Should Be Located")
 }
 
 function saveWallpaper()
 {
-    fs.stat(imgName, function(err, stat) {
+    fs.stat(imgName, async function(err, stat) {
         if(err == null)
         {
             fs.unlinkSync(path);
-            setImgName();
+            await setImgName();
+            console.log("2. Image Should Be Saved");
+            setWallpaper();
         }
         else if(err.code == 'ENOENT')
         {
-            setImgName();
+            await setImgName();
+            console.log("2. Image Should Be Saved");
+            setWallpaper();
         }
     });
-
-    console.log("2. Image Should Be Saved");
-    setWallpaper();
 }
-=======
-var imgName = "weebgReferenceBackground.jpg";
-var path = pathToModule + "/" + imgName;
->>>>>>> 79d576bcb9cc33ba3e05e5c188008f0299067810
 
 function setWallpaper()
 {
-    console.log("Path To BG Image: " + path);
+    console.log("Path Is: " + path);
     wallpaper.set(path);
-    console.log("Background Successfully Changed!");
-
+    console.log("3. Image Should Be Set");
 }
 
-setWallpaper();
+saveWallpaper();
